619cdad7d12fa608aa12092d6ae390136e1fca33
liuxzh <liuxz@hvgroup.com.cn>
2020-10-14 10:15:29 +0800

    1、test update index message

--- src/main/java/com/web/DockerController.java
+++ src/main/java/com/web/DockerController.java
@@ -15,8 +15,8 @@ public class DockerController {
 
     @RequestMapping("/")
     public String index() {
-        log.info("Hello SpringBoot App + Jenkins from Docker!");
-        return "Hello SpringBoot App + Jenkins from Docker!";
+        log.info("Hello SpringBoot Jenkins from Docker!");
+        return "Hello SpringBoot Jenkins from Docker!";
     }
 
 }
